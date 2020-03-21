e674ca90a193bd2fb11e8abc10ddad691489490b
kazuma1989 <funifuni.1204@gmail.com>
2020-03-21 20:18:41 +0900

    feat: React-Router を導入

--- packages/react/src/App.tsx
+++ packages/react/src/App.tsx
@@ -1,5 +1,38 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 
 export function App() {
-  return <div>React</div>
+  return (
+    <BrowserRouter basename="/react/">
+      <div>
+        <div>React app</div>
+
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/page-1">Page 1</Link>
+          </li>
+          <li>
+            <Link to="/page-2">Page 2</Link>
+          </li>
+        </ul>
+      </div>
+
+      <Switch>
+        <Route path="/page-1">
+          <div>Page 1</div>
+        </Route>
+
+        <Route path="/page-2">
+          <div>Page 2</div>
+        </Route>
+
+        <Route path="/">
+          <div>Home</div>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
 }
